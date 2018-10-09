e16857e8714eae0cc897c28be70cbcad6b00f743
Laurent Loukopoulos <laurent.loukopoulos@gmail.com>
2018-10-09 23:35:19 +0200

    array mutation example

--- src/index.js
+++ src/index.js
@@ -1,60 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+var expect = require('expect');
+var deepfreeze = require('deep-freeze');
 
-// reducers
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
+/* 
+*** code : here we have to avoid mutations
+*/
+
+const addCounter = (list) => {
+  return [...list, 0];
 }
 
-const store = createStore(counter);
-
-// no business logic
-const Counter = ({
-  value,
-  onIncrement,
-  onDecrement
-}) => (
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-);
-
-const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() =>
-        store.dispatch({
-          type: 'INCREMENT'
-        })
-      }
-      onDecrement={() =>
-        store.dispatch({
-          type: 'DECREMENT'
-        })
-      }
-    />,
-    document.getElementById('root')
-  );
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+  ];
 };
 
-// runs any time the state changes
-store.subscribe(render);
-render();
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ];
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* tests */
+
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+
+  // blocks mutations
+  deepfreeze(listBefore);
+
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
+};
+
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  // blocks mutations
+  deepfreeze(listBefore);
+
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  // blocks mutations
+  deepfreeze(listBefore);
+
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
+/* execution */
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log('All tests passed !');
