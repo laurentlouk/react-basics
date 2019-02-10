5f28e64adc5bb60547a9d129e3a2038134f76eda
Laurent Loukopoulos <laurent.loukopoulos@gmail.com>
2019-02-10 17:12:06 +0100

    added current filyter to rend it static

--- src/index.js
+++ src/index.js
@@ -63,8 +63,13 @@ const store = createStore(todoApp);
 
 const FilterLink = ({
   filter,
+  currentFilter,
   children
 }) => {
+  if (filter === currentFilter) {
+    return <span>{children}</span>;
+  }
+  
   return (
     // eslint-disable-next-line
     <a href='#'
@@ -104,9 +109,13 @@ const getVisibleTodos = (
 let nextTodoId = 0;
 class TodoApp extends Component {
   render() {
+    const {
+      todos,
+      visibilityFilter
+    } = this.props;
     const visibleTodos = getVisibleTodos(
-      this.props.todos,
-      this.props.visibilityFilter
+      todos,
+      visibilityFilter
     );
     return (
       <div>
@@ -147,18 +156,21 @@ class TodoApp extends Component {
           {' '}
           <FilterLink
             filter='SHOW_ALL'
+            currentFilter={visibilityFilter}
           >
             All
           </FilterLink>
           {' '}
           <FilterLink
             filter='SHOW_ACTIVE'
+            currentFilter={visibilityFilter}
           >
             Active
           </FilterLink>
           {' '}
           <FilterLink
             filter='SHOW_COMPLETED'
+            currentFilter={visibilityFilter}
           >
             completed
           </FilterLink>
